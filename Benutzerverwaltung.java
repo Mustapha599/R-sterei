8e6c8f4f2f0e14d01cd795c9611d2b0369ed9494
belaoumu <belaoun.mustapha@outlook.de>
2025-03-28 10:42:29 +0100

    Merge remote-tracking branch 'origin/main'

--- Benutzerverwaltung.java
+++ Benutzerverwaltung.java
@@ -4,20 +4,22 @@ public class Benutzerverwaltung extends BinarySearchTree {
 
     private BinarySearchTree<Benutzerprofil> benutzerBaum;
 
+
     public Benutzerverwaltung(){
         benutzerBaum= new BinarySearchTree<>();
     }
 
     public void neuenBenutzerAnlegen(String pBenutzername, String pPw){
-
+    Benutzerprofil x;
+    x=new Benutzerprofil(pBenutzername, pPw);
     }
 
     public void nutzerLoeschen(String pBenutzername, String pPw){
-
+     this.remove(pBenutzername && pPw);
     }
 
     public void profilVoranden(String pBenutzername){
-
+     this.search(pBenutzername);
     }
 
 
